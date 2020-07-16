95835faae56dd70dae26f826d983360dea7300e2
RoundBridge <410308507@qq.com>
2020-07-16 22:33:22 +0800

    modify the file mtools.py

--- math/mtools.py
+++ math/mtools.py
@@ -1,12 +1,52 @@
 '''
-    description:    计算x的n次方
+    description:    生成斐波那契序列内部函数，使用了yield，故该函数是生成器函数，
+                    返回一个生成器
     version:        0.0.1
     date:           2020.7.16
 '''
+def _fibonacci_(n):
+    a, b, counter = 0, 1, 0
+    while True:
+        if counter > n:
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+'''
+    description:    生成斐波那契序列外部调用函数，返回一个list
+    version:        0.0.1
+    date:           2020.7.16
+'''
+def fibonacci(n):
+    ret = []
+    g = _fibonacci_(n)
 
-def power(x,n):
-    s = 1
-    while n > 0:
-        n = n - 1
-        s = s * x
-    return s
+    while True:
+        try:
+            ele = next(g)
+            ret.append(ele)
+        except StopIteration:
+            return ret
+
+
+'''
+    description:    判断是否是闰年，返回True or False
+    version:        0.0.1
+    date:           2020.7.16
+'''
+def is_leap_year(year):
+    if 0 == (year % 100):
+        year = year/100
+        if 0 == (year % 4):
+            return True
+    elif 0 == (year % 4):
+        return True
+    else:
+        pass
+    return False
+
+
+
+
+if __name__ == '__main__':
+    print((is_leap_year(1904)))
