46078283c0132476c8657ba48d82372e66275ae3
RoundBridge <410308507@qq.com>
2020-07-30 20:06:01 +0800

    分数显示用逗号间隔，得分随等级难度相应增加

--- alien/settings.py
+++ alien/settings.py
@@ -16,6 +16,7 @@ class Settings():
 		self.fleet_drop_speed = 0.2
 
 		self.speedup_scale = 1.1  # 以什么样的速度加快游戏节奏
+		self.score_scale = 1.5    # 外星人点数的提高速度
 		self.initialize_dynamic_settings()
 
 
@@ -33,4 +34,5 @@ class Settings():
 		self.ship_speed_factor *= self.speedup_scale
 		self.bullet_speed_factor *= self.speedup_scale
 		self.alien_speed_factor *= self.speedup_scale
+		self.alien_points = int(self.alien_points * self.score_scale)
 
