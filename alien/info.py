48cd32a0107b2a4fc596974b34626af925b5bb68
RoundBridge <410308507@qq.com>
2020-08-13 17:09:40 +0800

    add event reponse

--- alien/info.py
+++ alien/info.py
@@ -13,9 +13,13 @@ SCREEN_COLOR = (100, 100, 100)
 
 class TextBox():
     def __init__(self, rect, **kwargs):
-        self.rect = pygame.Rect(rect)  # Rect(left, top, width, height) -> Rect or Rect((left, top), (width, height)) -> Rect
+        # Rect(left, top, width, height) -> Rect
+        # Rect((left, top), (width, height)) -> Rect
+        # Rect(object) -> Rect
+        self.rect = pygame.Rect(rect)
+        self.active = False  # 表示该TextBox当前是否被鼠标选中或者处于编辑状态
         self.buffer = []
-        self.final = None
+        self.final = None    # 表示该TextBox最终存储的字符串
         self.blink = True
         self.blink_timer = 0.0
         self.process_kwargs(kwargs)
@@ -23,11 +27,11 @@ class TextBox():
 
 
     def process_kwargs(self, kwargs):
-        defaults = {"msg" : " ",
-                    "bg_color" : pygame.Color("white"),
+        defaults = {"msg": " ",
+                    "bg_color": pygame.Color("white"),
                     "label_font_color": (150, 150, 150),
-                    "font_color" : pygame.Color("black"),
-                    "font" : pygame.font.Font(None, self.rect.height+4)}
+                    "font_color": pygame.Color("black"),
+                    "font": pygame.font.Font(None, self.rect.height+4)}
         for kwarg in kwargs:
             defaults[kwarg] = kwargs[kwarg]
         self.__dict__.update(defaults)
@@ -39,20 +43,21 @@ class TextBox():
         self.box_label_rect = self.box_label.get_rect()
         self.box_label_rect.left = self.rect.left
         self.box_label_rect.top = self.rect.top
-        self.screen.fill(self.bg_color, self.rect)  # 先绘制一个用颜色填充的输入框
-        self.screen.blit(self.box_label, self.box_label_rect)  # 再将标签提示文字绘制到输入框左端
+        self.screen.fill(self.bg_color, self.rect)#先绘制一个用颜色填充的输入框
+        self.screen.blit(self.box_label, self.box_label_rect)#再将标签提示文字绘制到输入框左端
 
 
 class Login():
     def __init__(self):
         self.screen = self.prep_screen()
         self.done = False
-        self.user = TextBox((SCREEN_W//2, 10, 150, 20), msg="User", screen=self.screen)
-        self.password = TextBox((SCREEN_W//2, 60, 150, 20), msg="Password", screen=self.screen)
-        self.register = TextBox((SCREEN_W//2, 110, 150, 20), msg="Register", screen=self.screen)
-        self.anonymous = TextBox((SCREEN_W//2, 160, 150, 20), msg="Anonymous", screen=self.screen)
-        self.login = TextBox((SCREEN_W//2, 210, 150, 20), msg="Login", screen=self.screen)
-        # pg.key.set_repeat(*KEY_REPEAT_SETTING)  #加*号可以去掉KEY_REPEAT_SETTING外面的括号
+        self.active_text_box = " "
+        self.text_box_obj = {}
+        self.text_box_obj["user"] = TextBox((SCREEN_W//2, 10, 150, 20), msg="User", screen=self.screen)
+        self.text_box_obj["password"] = TextBox((SCREEN_W//2, 60, 150, 20), msg="Password", screen=self.screen)
+        self.text_box_obj["register"] = TextBox((SCREEN_W//2, 110, 150, 20), msg="Register", screen=self.screen)
+        self.text_box_obj["anonymous"] = TextBox((SCREEN_W//2, 160, 150, 20), msg="Anonymous", screen=self.screen)
+        self.text_box_obj["login"] = TextBox((SCREEN_W//2, 210, 150, 20), msg="Login", screen=self.screen)
 
     def prep_screen(self):
         pygame.init()
@@ -61,24 +66,52 @@ class Login():
         screen.fill(SCREEN_COLOR)
         return screen
 
-    # def get_event(self, event):
-    #     if event.type == pygame.KEYDOWN and self.active:
-    #         if event.key in (pygame.K_RETURN, pygame.K_KP_ENTER):
-    #             self.execute()
-    #         elif event.key == pygame.K_BACKSPACE:
-    #             if self.buffer:
-    #                 self.buffer.pop()
-    #         elif event.unicode in VALID_CHAR:
-    #             self.buffer.append(event.unicode)
-    #     elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  #鼠标左键按下
-    #         self.active = self.rect.collidepoint(event.pos)
+    def update_mouse_click_info(self, pos):
+        for key in self.text_box_obj.keys():
+            self.text_box_obj[key].active = self.text_box_obj[key].rect.collidepoint(pos)
+
+    def is_text_box_selected(self):
+        ret = False
+        for key in self.text_box_obj.keys():
+            if self.text_box_obj[key].active:
+                ret = True
+                break
+        return ret
+
+    def find_text_box_selected(self):
+        ret = ""
+        for key in self.text_box_obj.keys():
+            if self.text_box_obj[key].active:
+                ret = key
+                break
+        return ret
+
+    def execute(self, active_box):
+        self.text_box_obj[active_box].final = "".join(self.text_box_obj[active_box].buffer)
+        print("final string: ", self.text_box_obj[active_box].final)
+
+    def proc_event(self):
+        pygame.key.set_repeat(*KEY_REPEAT_SETTING)  #加*号去掉KEY_REPEAT_SETTING外面的括号
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN and self.is_text_box_selected():
+                active_box = self.find_text_box_selected()
+                print(active_box)
+                if event.key in (pygame.K_RETURN, pygame.K_KP_ENTER):
+                    self.execute(active_box)
+                elif event.key == pygame.K_BACKSPACE:
+                    if self.text_box_obj[active_box].buffer:
+                        self.text_box_obj[active_box].buffer.pop()
+                elif event.unicode in VALID_CHAR:
+                    self.text_box_obj[active_box].buffer.append(event.unicode)
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: #左键按下
+                self.update_mouse_click_info(event.pos)
 
 
 if __name__ == '__main__':
     login = Login()
     while True:
         pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        login.proc_event()
 
