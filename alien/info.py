518f391710cbce542c57faca74d8eaf31503b227
RoundBridge <410308507@qq.com>
2020-08-14 19:41:54 +0800

    font bold display, adjust the places of three text boxes, enable respond to and receive of user's input

--- alien/info.py
+++ alien/info.py
@@ -18,12 +18,12 @@ class TextBox():
         # Rect(object) -> Rect
         self.rect = pygame.Rect(rect)
         self.active = False  # 表示该TextBox当前是否被鼠标选中或者处于编辑状态
-        self.buffer = []     # 键盘输入过程中作为临时列表保存输入的字符，最后会将buffer中的字符转化到final中
-        self.final = None    # 表示该TextBox最终存储的字符串
+        self.buffer = []     # 键盘输入过程中作为临时列表保存输入的字符，会将其中的字符转化到final中的字符串
+        self.final = None    # 表示该TextBox当前存储的字符串
         self.blink = True
         self.blink_timer = 0.0
         self.process_kwargs(kwargs)
-        self.make_box_label()
+        self.text_box_init()
 
 
     def process_kwargs(self, kwargs):
@@ -31,28 +31,61 @@ class TextBox():
                     "bg_color": pygame.Color("white"),
                     "label_font_color": (150, 150, 150),
                     "font_color": pygame.Color("black"),
-                    "font": pygame.font.Font(None, self.rect.height+4)}
+                    "font": " "}
         for kwarg in kwargs:
             defaults[kwarg] = kwargs[kwarg]
         self.__dict__.update(defaults)
 
-    def make_box_label(self):
+    def text_box_init(self):
         font_size = 18
+        font_bold = False
         message = self.msg
-        print(message)
         if message != "User" and message != "Password":
             if message == "Login":
                 font_size = 32
-        self.font = pygame.font.SysFont("arial", font_size)
-        self.box_label = self.font.render(message, True, self.label_font_color)
+                font_bold = True
+        self.font = pygame.font.SysFont("Arial", font_size)
+        self.font.set_bold(font_bold)
+        self.box_label = self.font.render(message, True, self.label_font_color, self.bg_color)
         self.box_label_rect = self.box_label.get_rect()
-        self.box_label_rect.left = self.rect.left
+        self.box_label_rect.left = self.rect.left+2
         self.box_label_rect.centery = self.rect.centery
         if message != "User" and message != "Password":
             # 除了User和Password，其余的居中显示
             self.box_label_rect.center = self.rect.center
         self.screen.fill(self.bg_color, self.rect)#先绘制一个用颜色填充的输入框
         self.screen.blit(self.box_label, self.box_label_rect)#再将标签提示文字绘制到输入框左端
+
+    def update_text_box_info(self):
+        new = "".join(self.buffer)
+        if new != self.final:
+            self.final = new
+            self.text = self.font.render(self.final, True, self.font_color, self.bg_color)
+            self.text_rect = self.text.get_rect(x=self.rect.x+2, centery=self.rect.centery)
+            if self.text_rect.width > self.rect.width - 6:
+                offset = self.text_rect.width - (self.rect.width - 6)
+                self.text_display_rect = pygame.Rect(offset, 0, self.rect.width - 6, self.text_rect.height)
+            else:
+                self.text_display_rect = self.text.get_rect(topleft=(0, 0))
+
+        if pygame.time.get_ticks() - self.blink_timer > 400:
+            self.blink = not self.blink
+            self.blink_timer = pygame.time.get_ticks()
+
+    def draw_text_box(self, surface):
+        surface.fill(self.bg_color, self.rect)  #先清除掉上一次绘制的内容
+        if not self.active:
+            self.screen.blit(self.box_label, self.box_label_rect) # 未被选中的情况下仍旧显示标签信息
+        if self.text:
+            # blit(source, dest, area=None, special_flags=0) -> Rect
+            # An optional area rectangle can be passed as well. This
+            # represents a smaller portion of the source Surface to draw.
+            surface.blit(self.text, self.text_rect, self.text_display_rect)
+        if self.blink and self.active:
+            # copy() -> Surface  Makes a duplicate copy of a Surface.
+            curse = self.text_display_rect.copy()
+            curse.topleft = self.text_rect.topleft
+            surface.fill(self.font_color, (curse.right+1, curse.y, 2, curse.h))
 
 
 class Login():
@@ -63,9 +96,9 @@ class Login():
         self.text_box_obj = {}
         self.text_box_obj["user"] = TextBox((SCREEN_W * 3 // 5, 80, 180, 25), msg="User", screen=self.screen)
         self.text_box_obj["password"] = TextBox((SCREEN_W * 3 // 5, 120, 180, 25), msg="Password", screen=self.screen)
-        self.text_box_obj["register"] = TextBox((SCREEN_W * 3 // 5, 160, 80, 20), msg="Register", screen=self.screen)
-        self.text_box_obj["anonymous"] = TextBox((SCREEN_W * 3 // 5, 195, 80, 20), msg="Anonymous", screen=self.screen)
-        self.text_box_obj["login"] = TextBox((SCREEN_W * 3 // 5 + 100, 160, 80, 55), msg="Login", screen=self.screen)
+        self.text_box_obj["register"] = TextBox((SCREEN_W * 3 // 5, 160, 80, 25), msg="Register", screen=self.screen)
+        self.text_box_obj["anonymous"] = TextBox((SCREEN_W * 3 // 5, 195, 80, 25), msg="Anonymous", screen=self.screen)
+        self.text_box_obj["login"] = TextBox((SCREEN_W * 3 // 5 + 90, 160, 90, 60), msg="Login", screen=self.screen)
 
     def prep_screen(self):
         pygame.init()
@@ -105,7 +138,6 @@ class Login():
                 sys.exit()
             elif event.type == pygame.KEYDOWN and self.is_text_box_selected():
                 active_box = self.find_text_box_selected()
-                print(active_box)
                 if event.key in (pygame.K_RETURN, pygame.K_KP_ENTER):
                     self.execute(active_box)
                 elif event.key == pygame.K_BACKSPACE:
@@ -113,11 +145,14 @@ class Login():
                         self.text_box_obj[active_box].buffer.pop()
                 elif event.unicode in VALID_CHAR:
                     self.text_box_obj[active_box].buffer.append(event.unicode)
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: #左键按下
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: #鼠标左键按下
                 self.update_mouse_click_info(event.pos)
 
     def update_screen(self):
-
+        for key in self.text_box_obj.keys():
+            if key == "user" or key == "password":
+                self.text_box_obj[key].update_text_box_info()
+                self.text_box_obj[key].draw_text_box(self.screen)
         pygame.display.update()
 
 
